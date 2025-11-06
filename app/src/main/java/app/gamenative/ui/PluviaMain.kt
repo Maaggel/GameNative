8d78547680614b473451d4a21e9e5ecce6f48683
Mikkel Bjørnmose Bundgaard <mikkel@kleangroup.dk>
2025-11-06 16:16:34 +0100

    Added topbar setting

--- app/src/main/java/app/gamenative/ui/PluviaMain.kt
+++ app/src/main/java/app/gamenative/ui/PluviaMain.kt
@@ -301,9 +301,11 @@ fun PluviaMain(
             // Log.d("PluviaMain", "Screen changed to $currentScreen, resetting some values")
             // TODO: remove this if statement once XServerScreen orientation change bug is fixed
             if (state.currentScreen != PluviaScreen.XServer) {
-                // reset system ui visibility
-                PluviaApp.events.emit(AndroidEvent.SetSystemUIVisibility(true))
-                // TODO: add option for user to set
+                // reset system ui visibility based on user preference
+                // If hideStatusBarWhenNotInGame is true, hide the status bar (visible = false)
+                // If false, show the status bar (visible = true)
+                val shouldShowStatusBar = !PrefManager.hideStatusBarWhenNotInGame
+                PluviaApp.events.emit(AndroidEvent.SetSystemUIVisibility(shouldShowStatusBar))
                 // reset available orientations
                 PluviaApp.events.emit(AndroidEvent.SetAllowedOrientation(EnumSet.of(Orientation.UNSPECIFIED)))
             }
